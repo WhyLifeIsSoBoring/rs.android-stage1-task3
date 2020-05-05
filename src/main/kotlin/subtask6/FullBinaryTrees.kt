dc0ea4d2e478628f9a54bcf1b5505ce17e52d7b7
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-05-05 23:33:17 +0300

    feat: add subtask6 solution

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,26 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
+    private fun trees(nach: String, symb: Int, free: Int): ArrayList<String> {
+        if (symb==0) {
+            return arrayListOf("$nach]")
+        } else {
+            val temp = trees("$nach,0,0", symb - 2, free+1)
+            if (free > 0) {
+                temp.addAll(trees("$nach,null,null", symb, free - 1))
+            }
+            return temp
+        }
+    }
+
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        var result ="["
+        if (count%2==1) {
+            for (tree in trees("[0", count - 1, 0)) {
+                result += tree
+            }
+        }
+        result+="]"
+        return result
     }
 }
