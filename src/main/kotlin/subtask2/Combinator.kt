2ca898cab9d45871b7d07486d6d2c1c2286f9dc9
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-05-05 20:37:08 +0300

    feat: add subtask2 solution

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,20 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val numberOfPosters = array[0]
+        var numberOfAvailableColors = array[1]
+        var combinations = 1
+
+        for(numberOfColorsForEachPoster in 1..numberOfAvailableColors) {
+            
+            combinations = combinations * numberOfAvailableColors / numberOfColorsForEachPoster
+
+            if( numberOfPosters == combinations ) {
+                return numberOfColorsForEachPoster
+            }
+            numberOfAvailableColors -= 1
+        }
+        return null
     }
 }
